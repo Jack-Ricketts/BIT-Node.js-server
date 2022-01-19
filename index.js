22f8c4062a184d3bc1e501e1d723131ea6fda17c
Jack-Ricketts <zygga.k@gmail.com>
2022-01-19 20:37:33 +0200

    server

---
+++ index.js
@@ -0,0 +1,9 @@
+/* 
+[projekto pavadinimas] [aplinkos pavadinimas] [default kalba]
+
+Jeigu kodas pasileidzia is programuotojo PC, tai:
+Server dev en
+
+Jeigu kodas pasileidzia is programuotojo PC, tai:
+Serveris production lt
+*/ 
